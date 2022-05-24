b934fac91f9559fcc5164d06ea69998796cb4a11
Gsha888 <76055722+Gsha888@users.noreply.github.com>
2022-05-24 15:05:48 +0300

    Create normalize.py

---
+++ Homework7/clean_folder/normalize.py
@@ -0,0 +1,16 @@
+
+import re
+
+CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфчцшщъыьэюя"
+TRANSLATION = ("a","b","v","g","d","e","e","j","z","i","j","k","l","m","n","o","p","r","s","t","u","f","ch","c","sh","sch","","y","","e", "yu","ya")      
+
+
+TRANS = {}
+for c,l in zip(CYRILLIC_SYMBOLS,TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper
+    
+def normalize(name: str) -> str:
+    t_name = name.translate(TRANS)
+    t_name = re.sub(r'\W', "_", t_name)
+    return t_name
